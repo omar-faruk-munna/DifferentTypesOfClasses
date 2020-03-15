d82b99530ffdd77c6b71c25cea6b8172e67fbd16
Omar Faruk Munna <omarmunna67@outlook.com>
2020-03-15 20:07:44 +0600

    change

---
+++ DifferentTypesOfClasses/Static Class/StaticClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DifferentTypesOfClasses.Static_Class
+{
+    public interface IStatic
+    {
+        void save();
+    }
+
+    public class MyClass 
+    {
+        public static string name;
+    }
+}
